668b0bdb9a4f1ca5169b5ea222b566267ba3ea2e
Koykoy200078 <franc200078@gmail.com>
2025-03-17 14:59:57 +0800

    update: class outside the mainactivity

--- MainActivity.cs
+++ MainActivity.cs
@@ -6,34 +6,30 @@ using System.Threading.Tasks;
 
 namespace COMPROG2
 {
-    class Market 
-    {
-        public int Price { get; set; }
-    }
-
     class MainActivity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Select (1-3): ");
-            int num = Convert.ToInt32(Console.ReadLine());
+            Student std = new Student();
+            Student std2 = new Student();
 
-            switch(num)
-            {
-                case 1:
-                    Console.WriteLine("This is 1");
-                    break;
-                case 2:
-                    Console.WriteLine("This is 2");
-                    break;
-                case 3:
-                    Console.WriteLine("This is 3");
-                    break;
+            std.name = "John";
+            std.age = 18;
+            std.gender = "Male";
 
-                default:
-                    break;
-            }
+            std2.name = "Jon";
+            std2.age = 81;
+            std2.gender = "Male";
 
+            Console.WriteLine("Student 1");
+            Console.WriteLine("Name: " + std.name);
+            Console.WriteLine("Age: " + std.age);
+            Console.WriteLine("Gender: " + std.gender);
+            Console.WriteLine("==================================");
+            Console.WriteLine("Student 2");
+            Console.WriteLine("Name: " + std2.name);
+            Console.WriteLine("Age: " + std2.age);
+            Console.WriteLine("Gender: " + std2.gender);
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMPROG2
+{
+    class Student
+    {
+        public string name;
+        public int age;
+        public string gender;
+
+        public static int studentCount;
+    }
+}
